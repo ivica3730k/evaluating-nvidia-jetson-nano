42fa9a2f321d76d843c0f68d772668c7295f8961
Ivica <ivica-matic@outlook.com>
2022-01-17 16:10:42 +0000

    Fixed gpio trigger

--- fps_benchmark_photo_singleshot.py
+++ fps_benchmark_photo_singleshot.py
@@ -27,7 +27,7 @@ else:
     GPIO.setmode(GPIO.BCM)  # BCM pin-numbering scheme from Raspberry Pi
     GPIO.setup(input_pin, GPIO.IN)  # set pin as an input pin
     print("Waiting for pin trigger")
-    while GPIO.input(input_pin) == GPIO.low:
+    while GPIO.input(input_pin) == GPIO.LOW:
         continue
     t = time.process_time()
     objs = Object_detector.detect(frame)
