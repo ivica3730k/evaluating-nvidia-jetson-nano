1c50a2658e389f8c69fe0cce1bc8fb5427206a55
Ivica <ivica-matic@outlook.com>
2022-01-17 21:41:10 +0000

    Updated video benchmark

--- fps_benchmark_video.py
+++ fps_benchmark_video.py
@@ -3,6 +3,11 @@ import time
 
 import cv2
 
+use_gpio = True
+try:
+    import RPi.GPIO as GPIO
+except ImportError:
+    use_gpio = False
 from yolov5 import ObjectDetection
 
 RED_COLOR = (255, 0, 0)
@@ -19,18 +24,42 @@ print("Starting inference")
 samples = 0
 measuring_samples = 0
 total_time = 0.000
-while True:
-    # print("Inference starting:")
-    ok, frame = camera.read()
-    if not ok:
-        print("Error reading camera")
-        exit(1)
-    t = time.process_time()
-    objs = Object_detector.detect(frame)
-    frame_time = round(time.process_time() - t, 5)
-    print("Frame time: ", frame_time)
-    samples += 1
-    if samples >= 60:
+
+if not use_gpio:
+    while True:
+        ok, frame = camera.read()
+        if not ok:
+            print("Error reading camera")
+            exit(1)
+        t = time.process_time()
+        objs = Object_detector.detect(frame)
+        frame_time = round(time.process_time() - t, 5)
+        print("Frame time: ", frame_time)
+        samples += 1
         measuring_samples += 1
         total_time += frame_time
         print("Avg Frame Time: ", round(total_time / measuring_samples, 5))
+else:
+    input_pin = 18  # BCM pin 18, BOARD pin 12
+    GPIO.setmode(GPIO.BCM)  # BCM pin-numbering scheme from Raspberry Pi
+    GPIO.setup(input_pin, GPIO.IN)  # set pin as an input pin
+    while True:
+        while GPIO.input(input_pin) == GPIO.HIGH:
+            continue
+        while GPIO.input(input_pin) == GPIO.LOW:
+            ok, frame = camera.read()
+            if not ok:
+                print("Error reading camera")
+                exit(1)
+            t = time.process_time()
+            objs = Object_detector.detect(frame)
+            frame_time = round(time.process_time() - t, 5)
+            print("Frame time: ", frame_time)
+            samples += 1
+            measuring_samples += 1
+            total_time += frame_time
+        print("Avg Frame Time: ", round(total_time / measuring_samples, 5))
+        samples = 0
+        measuring_samples = 0
+        total_time = 0.000
+        camera = cv2.VideoCapture(sys.argv[1])
