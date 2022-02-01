ba1755493e3fd8f87a0dfffb018b7bc9a11e7f78
Ivica <ivica-matic@outlook.com>
2022-02-01 18:41:13 +0000

    Created new benchmark programs

--- fps_benchmark_photo.py
+++ fps_benchmark_photo.py
@@ -1,3 +1,4 @@
+import glob
 import sys
 import time
 
@@ -10,29 +11,33 @@ except ImportError:
     use_gpio = False
 from yolov5 import ObjectDetection
 
-print("Loading weigths!")
-Object_detector = ObjectDetection.ObjectDetection('weights/' + sys.argv[2], input_width=640)
-print("Starting inference")
-frame = cv2.imread(sys.argv[1])
-frame = cv2.resize(frame, (640, 480), interpolation=cv2.INTER_AREA)
+images = []
+for file in glob.glob("./Coco2017_val/*"):
+    images.append(cv2.imread(file))
 
-for i in range(0, 4):
+print("Loading weigths!")
+Object_detector = ObjectDetection.ObjectDetection(sys.argv[1], input_width=640)
+print("Starting inference - predetect")
+
+for i in range(0, 10):
     # Detect image few times because it is slow first few times
-    objs = Object_detector.detect(frame)
+    objs = Object_detector.detect(images[i])
+
+print("Starting inference")
 
 samples = 0
-measuring_samples = 0
 total_time = 0.000
 if not use_gpio:
     while True:
-        t = time.process_time()
-        objs = Object_detector.detect(frame)
-        frame_time = round(time.process_time() - t, 5)
-        print("Frame time: ", frame_time)
-        samples += 1
-        measuring_samples += 1
-        total_time += frame_time
-        print("Avg Frame Time: ", round(total_time / measuring_samples, 5))
+        for frame in images:
+            t = time.time()
+            objs = Object_detector.detect(frame)
+            frame_time = round(time.time() - t, 5)
+            # print("Frame time: ", frame_time)
+            samples += 1
+            total_time += frame_time
+            print("Avg Frame Time: ", round(total_time / samples, 5))
+            print("Average framerate:", round(samples / total_time, 2))
 else:
     input_pin = 18  # BCM pin 18, BOARD pin 12
     GPIO.setmode(GPIO.BCM)  # BCM pin-numbering scheme from Raspberry Pi
@@ -41,14 +46,17 @@ else:
         while GPIO.input(input_pin) == GPIO.HIGH:
             continue
         while GPIO.input(input_pin) == GPIO.LOW:
-            t = time.process_time()
-            objs = Object_detector.detect(frame)
-            frame_time = round(time.process_time() - t, 5)
-            print("Frame time: ", frame_time)
-            samples += 1
-            measuring_samples += 1
-            total_time += frame_time
-        print("Avg Frame Time: ", round(total_time / measuring_samples, 5))
+            for frame in images:
+                if GPIO.input(input_pin) == GPIO.HIGH:
+                    break
+                t = time.time()
+                objs = Object_detector.detect(frame)
+                frame_time = round(time.time() - t, 5)
+                print("Frame time: ", frame_time)
+                samples += 1
+                total_time += frame_time
+        print("Avg Frame Time: ", round(total_time / samples, 5))
+        print("Total samples processed: ", samples)
+        print("Average framerate:", round(samples / total_time, 2))
         samples = 0
-        measuring_samples = 0
         total_time = 0.000
