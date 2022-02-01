1aaa89451c33b5238013eed377696eba0c869279
Ivica <ivica-matic@outlook.com>
2022-02-01 19:03:07 +0000

    Updated testvideo function

--- fps_benchmark_video.py
+++ fps_benchmark_video.py
@@ -18,7 +18,7 @@ if not ok:
     print("Error reading camera")
     exit(1)
 print("Loading weigths!")
-Object_detector = ObjectDetection.ObjectDetection('weights/' + sys.argv[2], input_width=640)
+Object_detector = ObjectDetection.ObjectDetection(sys.argv[2], input_width=640)
 print("Starting inference")
 
 samples = 0
@@ -31,14 +31,14 @@ if not use_gpio:
         if not ok:
             print("Error reading camera")
             exit(1)
-        t = time.process_time()
+        t = time.time()
         objs = Object_detector.detect(frame)
-        frame_time = round(time.process_time() - t, 5)
-        print("Frame time: ", frame_time)
+        frame_time = round(time.time() - t, 5)
+        # print("Frame time: ", frame_time)
         samples += 1
-        measuring_samples += 1
         total_time += frame_time
-        print("Avg Frame Time: ", round(total_time / measuring_samples, 5))
+        print("Avg Frame Time: ", round(total_time / samples, 5))
+        print("Average framerate:", round(samples / total_time, 2))
 else:
     input_pin = 18  # BCM pin 18, BOARD pin 12
     GPIO.setmode(GPIO.BCM)  # BCM pin-numbering scheme from Raspberry Pi
@@ -51,14 +51,15 @@ else:
             if not ok:
                 print("Error reading camera")
                 exit(1)
-            t = time.process_time()
-            objs = Object_detector.detect(frame)
-            frame_time = round(time.process_time() - t, 5)
-            print("Frame time: ", frame_time)
-            samples += 1
-            measuring_samples += 1
-            total_time += frame_time
-        print("Avg Frame Time: ", round(total_time / measuring_samples, 5))
+                t = time.time()
+                objs = Object_detector.detect(frame)
+                frame_time = round(time.time() - t, 5)
+                print("Frame time: ", frame_time)
+                samples += 1
+                total_time += frame_time
+        print("Avg Frame Time: ", round(total_time / samples, 5))
+        print("Total samples processed: ", samples)
+        print("Average framerate:", round(samples / total_time, 2))
         samples = 0
         measuring_samples = 0
         total_time = 0.000
