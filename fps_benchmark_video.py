46889c6206a3f29e4ca8d431eb20045da6d2e153
Ivica <ivica-matic@outlook.com>
2022-02-02 12:32:48 +0000

    Loading video into ram for inference

--- fps_benchmark_video.py
+++ fps_benchmark_video.py
@@ -17,28 +17,28 @@ ok, frame = camera.read()
 if not ok:
     print("Error reading camera")
     exit(1)
+print("Loading video into ram")
+images = []
+for i in range(0, 1000):
+    ok, frame = camera.read()
+    images.append(frame)
 print("Loading weigths!")
 Object_detector = ObjectDetection.ObjectDetection(sys.argv[2], input_width=640)
 print("Starting inference")
 
 samples = 0
-measuring_samples = 0
 total_time = 0.000
-
 if not use_gpio:
     while True:
-        ok, frame = camera.read()
-        if not ok:
-            print("Error reading camera")
-            exit(1)
-        t = time.time()
-        objs = Object_detector.detect(frame)
-        frame_time = round(time.time() - t, 5)
-        # print("Frame time: ", frame_time)
-        samples += 1
-        total_time += frame_time
-        print("Avg Frame Time: ", round(total_time / samples, 5))
-        print("Average framerate:", round(samples / total_time, 2))
+        for frame in images:
+            t = time.time()
+            objs = Object_detector.detect(frame)
+            frame_time = round(time.time() - t, 5)
+            # print("Frame time: ", frame_time)
+            samples += 1
+            total_time += frame_time
+            print("Avg Frame Time: ", round(total_time / samples, 5))
+            print("Average framerate:", round(samples / total_time, 2))
 else:
     input_pin = 18  # BCM pin 18, BOARD pin 12
     GPIO.setmode(GPIO.BCM)  # BCM pin-numbering scheme from Raspberry Pi
@@ -48,17 +48,17 @@ else:
         while GPIO.input(input_pin) == GPIO.HIGH:
             continue
         while GPIO.input(input_pin) == GPIO.LOW:
-            ok, frame = camera.read()
-            t = time.time()
-            objs = Object_detector.detect(frame)
-            frame_time = round(time.time() - t, 5)
-            print("Frame time: ", frame_time)
-            samples += 1
-            total_time += frame_time
+            for frame in images:
+                if GPIO.input(input_pin) == GPIO.HIGH:
+                    break
+                t = time.time()
+                objs = Object_detector.detect(frame)
+                frame_time = round(time.time() - t, 5)
+                print("Frame time: ", frame_time)
+                samples += 1
+                total_time += frame_time
         print("Avg Frame Time: ", round(total_time / samples, 5))
         print("Total samples processed: ", samples)
         print("Average framerate:", round(samples / total_time, 2))
         samples = 0
-        measuring_samples = 0
         total_time = 0.000
-        camera = cv2.VideoCapture(sys.argv[1])
