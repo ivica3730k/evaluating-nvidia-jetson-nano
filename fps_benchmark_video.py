1058f23cb0c02ac56508b067e71f553e0a29ff83
Ivica <ivica-matic@outlook.com>
2022-02-04 13:38:35 +0000

    Fixing benchmark program

--- fps_benchmark_video.py
+++ fps_benchmark_video.py
@@ -46,7 +46,7 @@ else:
     while True:
         print("Waiting for GPIO trigger")
         while GPIO.input(input_pin) == GPIO.HIGH:
-            time.sleep(0.001)
+            time.sleep(0.01)
             continue
         while GPIO.input(input_pin) == GPIO.LOW:
             if GPIO.input(input_pin) == GPIO.HIGH:
